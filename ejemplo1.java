5e4c4c8089b7242c5054031b6e53a4dee149c107
YoselinMA <162406301+YoselinMA@users.noreply.github.com>
2024-06-06 21:05:04 -0600

    Create ejemplo1.java

---
+++ ejemplo1.java
@@ -0,0 +1,14 @@
+public class ejemplo1 {
+    public static void main(String[] args) {
+        int a = 40;
+        int b = 20;
+        int c = 10;
+        int d = a+b+c;
+        float e = d / 3;
+
+        System.out.println("El resultado de la suma de los numeros es = "+ d);
+        System.out.println("El resultado de la divicion de la suma entre los numeros es = "+ e);
+
+    }
+    
+}
